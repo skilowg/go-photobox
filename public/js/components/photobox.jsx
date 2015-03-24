2e14f71f2bd88d412e72985d5d9ae11781211318
David Pierce <david.dean.pierce@gmail.com>
2015-03-24 10:45:28 -0700

    Photobox component tracks current parent directory
    As the user drills into folders, keeping track of which
    folder they're in allows PhotoBox to assemble the proper folder
    path for file listing requests
    
    TODO: Replace solution with 'displayName' and 'fullPath' fields

--- public/js/components/photobox.jsx
+++ public/js/components/photobox.jsx
@@ -1,16 +1,34 @@
 var Photobox = React.createClass({
   getInitialState: function () {
-    return { files: [] };
+    return {
+      files: [],
+      fileRoot: ''
+    };
   },
 
   loadFileData: function (path) {
-    var req = new XMLHttpRequest();
+    var req = new XMLHttpRequest(),
+        filePath = '';
 
     // Set up request to server for files at `path`
-    req.open("GET", "/files" + (path.length ? '?path=' + path : ''));
+
+    if (path.length) {
+      filePath = '?path=';
+      filePath += this.state.fileRoot.length ?
+        // Prepend the fileroot to the path if we have one
+        this.state.fileRoot + '/' + path :
+        // No fileroot, so just send the path
+        path;
+
+    } else {
+      filePath = '';
+    }
+
+    req.open("GET", "/files" + filePath);
     req.onreadystatechange = function (evt) {
       if (req.readyState === 4) {
         this.setState({
+          fileRoot: path,
           files: req.response.split(',')
         })
       }
@@ -27,7 +45,8 @@ var Photobox = React.createClass({
     return (
       <FileList
         files={this.state.files}
-        notifyPathChange={this.loadFileData} />
+        notifyPathChange={this.loadFileData}
+        fileRoot={this.state.fileRoot} />
     );
   }
 });
